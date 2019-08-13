f3f680a390fe6756f98907820cd43e0438f42810
shehzadbrohi <shahzadbrohi@hotmail.com>
2019-08-14 02:34:46 +0500

    admin panel in react

---
+++ src/users.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Create, Edit, List, Datagrid, TextField, EmailField, UrlField, DisabledInput, SimpleForm, TextInput } from 'react-admin';
+export const UserList = props => (
+    <List {...props}>
+        <Datagrid rowClick="edit">
+            <TextField source="id" />
+            <TextField source="name" />
+            <TextField source="username" />
+            <EmailField source="email" />
+            <TextField source="address.street" label="Address" />
+            <TextField source="phone" />
+            <UrlField source="website" />
+            <TextField source="company.name" label="Company" />
+        </Datagrid>
+    </List>
+);
+
+export const UserEdit = props => (
+    <Edit {...props}>
+      <SimpleForm>
+        <DisabledInput source="id" />
+        <TextInput source="name" />
+        <TextInput source="username" />
+        <TextInput source="email" />
+        <TextInput source="address.street" label="Address" />
+        <TextInput source="phone" />
+        <UrlField source="website" />
+        <TextInput source="company.name" label="Company" />
+      </SimpleForm>
+    </Edit>
+  );
+
+  export const UserCreate = props => (
+    <Create {...props}>
+      <SimpleForm>
+        <DisabledInput source="id" />
+        <TextInput source="name" />
+        <TextInput source="username" />
+        <TextInput source="email" />
+        <TextInput source="address.street" label="Address" />
+        <TextInput source="phone" />
+        <UrlField source="website" />
+        <TextInput source="company.name" label="Company" />
+      </SimpleForm>
+    </Create>
+  );
